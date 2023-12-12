11f23c693f3d31b6e1b12ddeabd57e9153e12459
LullabyOfLies <jeanlorenz.recato@tup.edu.ph>
2023-12-12 09:36:46 +0800

    Debuging backend pt11 | fixed directory with dot

--- backend/api/server.js
+++ backend/api/server.js
@@ -29,8 +29,8 @@ app.use((req,res,next)=>{
 })
 
 //routes
-app.use('/api/workouts',workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('../api/workouts',workoutRoutes)
+app.use('../api/user', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI) // is equal to connected to database first 
