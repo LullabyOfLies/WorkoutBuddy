649663516da3ad6c134a7ac24a39b5740bac4102
LullabyOfLies <jeanlorenz.recato@tup.edu.ph>
2023-12-12 09:55:07 +0800

    Debugging backend pt13 | configure server.js directories

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express');
-const workoutRoutes = require('../routes/workouts')
-const userRoutes = require('../routes/user')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
+const routes1 = require('../routes/')
 
 //express app
 const app = express()
@@ -29,8 +29,9 @@ app.use((req,res,next)=>{
 })
 
 //routes
-app.use('../api/workouts',workoutRoutes)
-app.use('../api/user', userRoutes)
+app.use('/api/workouts',workoutRoutes)
+app.use('/api/user', userRoutes)
+
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI) // is equal to connected to database first 
