d10ae8fbccd6ca195cc79a5c5358b3d3a03e8df9
LullabyOfLies <jeanlorenz.recato@tup.edu.ph>
2023-12-14 10:26:52 +0800

    trying chatgpt solution

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,46 @@
 const express = require('express');
-const workoutRoutes = require('./routes/workouts')
-const userRoutes = require('./routes/user')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const MONGO_URI = "mongodb+srv://DjinnLorenz:RecatoFamily97@mernapp.d588qnu.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = "mongodb+srv://DjinnLorenz:RecatoFamily97@mernapp.d588qnu.mongodb.net/?retryWrites=true&w=majority";
 
-const DOMAIN = "https://workout-buddy-api.vercel.app"
+const DOMAIN = "https://workout-buddy-api.vercel.app";
 
-//express app
-const app = express()
+// express app
+const app = express();
 
 app.get('/', (req, res) => {
-       res.send('Hello World')
-     })
+  res.send('Hello World');
+});
 
-//middleware
-app.use(cors(
-       {
-              origin: ["https://workout-buddy-two.vercel.app/login"],
-              methods : ["POST", "GET", "DELETE"],
-              credentials: true
-       }
-));
-app.use(express.json())
+// middleware
+app.use(cors({
+  origin: ["https://workout-buddy-two.vercel.app/login"],
+  methods: ["POST", "GET", "DELETE"],
+  credentials: true,
+}));
+app.use(express.json());
 
-app.use((req,res,next)=>{
-       console.log(req.path, req.method)
-       next();
-})
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-//routes
-app.use('/api/workouts',workoutRoutes)
-app.use('/api/user', userRoutes)
+// routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
-//connect to db
-mongoose.connect(MONGO_URI) // is equal to connected to database first 
-       .then(()=>{ 
-              //listen for request from a URL or port that will be sent to the database
-              app.listen(("/"), () => {
-                     console.log("connected to db and listening on port 3000!")
-              })
-       })
-       .catch((error)=>{
-              console.log(error)
-              console.log('There is an error Controller ');
-       })
-
-
+// connect to db
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    // listen for requests on a specific path
+    app.use("/", (req, res) => {
+      console.log("connected to db and listening");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log('There is an error Controller');
+  });
